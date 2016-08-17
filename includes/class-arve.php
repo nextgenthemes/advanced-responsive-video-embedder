cc36cbbad4af7d0771b6fe61876b89c4ea5bc599
Nicolas Jonas <nicolasjonas@web.de>
2016-08-17 20:51:41 +0200

    forgot zip

--- includes/class-arve.php
+++ includes/class-arve.php
@@ -45,7 +45,7 @@ class Advanced_Responsive_Video_Embedder {
 	public function __construct() {
 
 		$this->plugin_slug = 'advanced-responsive-video-embedder';
-		$this->version = '7.1.25';
+		$this->version = '7.1.26';
 
 		$this->load_dependencies();
 		$this->set_locale();
