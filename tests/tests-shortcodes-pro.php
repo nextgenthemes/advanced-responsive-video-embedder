246ef5d3d54a753c3427f73579edbf5d4744f1c5
Nicolas Jonas <nnico@protonmail.com>
2016-10-24 16:42:05 +0200

    FFS

--- tests/tests-shortcodes-pro.php
+++ tests/tests-shortcodes-pro.php
@@ -8,6 +8,7 @@ class Tests_Shortcode_Pro extends WP_UnitTestCase {
 		$this->assertTrue( is_plugin_active( 'arve-pro/arve-pro.php' ) );
 		$this->assertTrue( function_exists( 'arve_init' ) );
 		$this->assertTrue( function_exists( 'arve_pro_init' ) );
+		$this->assertTrue( function_exists( 'arve_pro_activation_hook' ) );
 		$this->assertTrue( function_exists( 'arve_pro_filter_modes' ) );
 
 		$filename = dirname( __FILE__ ) . '/test-attachment-2.jpg';
