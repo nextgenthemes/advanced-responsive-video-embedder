6f758b843403fc6382cb6fec03edc0cff4bef1d8
Nicolas Jonas <nnico@protonmail.com>
2016-11-05 10:30:09 +0100

    test updateC

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -122,8 +122,12 @@ install_db() {
 
 install_arve_pro() {
 	download https://nextgenthemes.com/travis-files/arve-pro-test.zip.enc /tmp/arve-pro-test.zip.enc
+	download https://nextgenthemes.com/travis-files/fake-activate.php.enc /tmp/fake-activate.php
+
 	openssl aes-256-cbc -k "$TRAVIS_FILES_PASS" -in /tmp/arve-pro-test.zip.enc -out /tmp/arve-pro-test.zip -d
-	unzip /tmp/arve-pro-test.zip -d /home/travis/build/nextgenthemes/
+	openssl aes-256-cbc -k "$TRAVIS_FILES_PASS" -in /tmp/fake-activate.php.enc -out /tmp/fake-activate.php -d
+
+	unzip /tmp/arve-pro-test.zip -d /tmp
 }
 
 install_wp
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -14,7 +14,7 @@ require_once $_tests_dir . '/includes/functions.php';
 function _manually_load_plugin() {
 	# /home/travis/build/nextgenthemes/advanced-responsive-video-embedder
 	require dirname( __FILE__ ) . '/../advanced-responsive-video-embedder.php';
-	require dirname( __FILE__ ) . '/../../arve-pro/arve-pro.php';
+	require dirname( __FILE__ ) . '/tmp/arve-pro/arve-pro.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
@@ -23,7 +23,7 @@ require $_tests_dir . '/includes/bootstrap.php';
 activate_plugin( 'advanced-responsive-video-embedder/advanced-responsive-video-embedder.php' );
 activate_plugin( 'arve-pro/arve-pro.php' );
 
-require dirname( __FILE__ ) . '/../../arve-pro/fake-activate.php';
+require dirname( __FILE__ ) . '/tmp/fake-activate.php';
 
 global $current_user;
 
