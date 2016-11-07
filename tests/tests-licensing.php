232efac6437c1d6c145dfae9af6ca0b071b89022
Nicolas Jonas <nnico@protonmail.com>
2016-11-07 12:53:13 +0100

    fix key tests

--- tests/tests-licensing.php
+++ tests/tests-licensing.php
@@ -12,17 +12,16 @@ class Tests_Licensing_Pro extends WP_UnitTestCase {
 		add_filter( 'nextgenthemes_products', array( $this, 'add_product' ) );
 
     update_option( 'nextgenthemes_example_product_key', 'key_in_option' );
-
     $this->assertEquals( nextgenthemes_get_key( 'example_product' ), 'key_in_option' );
 
-    define( 'EXAMPLE_PRODUCT_KEY', 'defined_key' );
-
-    $this->assertEquals( nextgenthemes_get_key( 'example_product' ), 'defined_key' );
+    define( 'EXAMPLE_PRODUCT_TWO_KEY', 'defined_key' );
+    $this->assertEquals( nextgenthemes_get_key( 'example_product_two' ), 'defined_key' );
 	}
 
 	public function add_product( $products ) {
 
-		$products['example_product'] = array();
+		$products['example_product']     = array();
+		$products['example_product_two'] = array();
 
 		return $products;
 	}
