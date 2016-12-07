a3405b7cad141b791c6bd627286e0f844eb97291
Nicolas Jonas <nnico@protonmail.com>
2016-12-07 23:04:29 +0100

    skip url test to prevent travis error

--- tests/tests-urls.php
+++ tests/tests-urls.php
@@ -5,6 +5,18 @@ class Tests_URLs extends WP_UnitTestCase {
 
 	public function test_urls() {
 
+		global $wp_version;
+
+		if(
+			5 == PHP_MAJOR_VERSION &&
+			3 == PHP_MINOR_VERSION &&
+			version_compare( $wp_version, '4.6', '<' )
+		) {
+			$this->markTestSkipped(
+				'Skip this because of error: GnuTLS recv error (-9): A TLS packet with unexpected length was received.'
+			);
+		}
+
     $output = apply_filters( 'the_content', 'https://www.youtube.com/watch?v=2an6-WVPuJU&arve[align]=left&arve[autoplay]=1&arve[maxwidth]=333&arve[title]=title' );
 
 		$this->assertNotContains( 'Error', $output );
