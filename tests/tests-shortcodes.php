6d0174fcca8c4861ece0000b65a2649da245a3a5
Nicolas Jonas <nnico@protonmail.com>
2017-10-15 10:36:48 +0200

    Remove sandbox tests for pro addon.

--- tests/tests-shortcodes.php
+++ tests/tests-shortcodes.php
@@ -13,11 +13,6 @@ class Tests_Shortcode extends WP_UnitTestCase {
 		$this->assertNotContains( 'Error', arve_shortcode_arve( $attr ) );
 		$this->assertContains( 'sandbox="allow-scripts allow-same-origin allow-presentation allow-popups"', arve_shortcode_arve( $attr ) );
 
-		$attr['disable_links'] = 'y';
-
-		$this->assertNotContains( 'Error', arve_shortcode_arve( $attr ) );
-		$this->assertContains( 'sandbox="allow-scripts allow-same-origin allow-presentation', arve_shortcode_arve( $attr ) );
-
 		$attr = array(
 			'url'       => 'https://vimeo.com/214300845',
 			'thumbnail' => 'https://example.com/image.jpg',
@@ -26,11 +21,6 @@ class Tests_Shortcode extends WP_UnitTestCase {
 
 		$this->assertNotContains( 'Error', arve_shortcode_arve( $attr ) );
 		$this->assertContains( 'sandbox="allow-scripts allow-same-origin allow-presentation allow-popups allow-forms"', arve_shortcode_arve( $attr ) );
-
-		$attr['disable_links'] = 'y';
-
-		$this->assertNotContains( 'Error', arve_shortcode_arve( $attr ) );
-		$this->assertContains( 'sandbox="allow-scripts allow-same-origin allow-presentation allow-forms"', arve_shortcode_arve( $attr ) );
 	}
 
 	public function test_thumbnails() {
