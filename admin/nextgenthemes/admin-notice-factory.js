b058ed55da964a447d43b8d74165907c70dda655
Nicolas Jonas <nnico@protonmail.com>
2017-10-16 12:15:18 +0200

    Fix notice dismissal

--- admin/nextgenthemes/admin-notice-factory.js
+++ admin/nextgenthemes/admin-notice-factory.js
@@ -1,13 +1,15 @@
 /*global ajaxurl */
 jQuery( function( $ ) {
 
-  var id = $(this).closest('[data-nj-notice-id]').attr('data-nj-notice-id');
+	$(document).on( 'click', '[data-nj-notice-id] .notice-dismiss', function() {
 
-  jQuery.ajax({
-    url: ajaxurl,
-    data: {
-      action: id
-    }
-  });
+		var id = $(this).closest('[data-nj-notice-id]').attr('data-nj-notice-id');
 
+		jQuery.ajax({
+			url: ajaxurl,
+			data: {
+				action: id
+			}
+		});
+	});
 } );
