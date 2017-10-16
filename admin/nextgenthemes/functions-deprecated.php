32008462ad1c8faae1780d9f143d29d99267b1ce
Nicolas Jonas <nnico@protonmail.com>
2017-10-16 02:19:49 +0200

    Add nextgenthemes_update_key_status to deprecated functions.

--- admin/nextgenthemes/functions-deprecated.php
+++ admin/nextgenthemes/functions-deprecated.php
@@ -7,3 +7,7 @@ function nextgenthemes_has_valid_key( $slug ) {
 function nextgenthemes_api_update_key_status( $slug, $key, $action ) {
   return nextgenthemes\admin\api_update_key_status( $slug, $key, $action );
 }
+
+function nextgenthemes_update_key_status( $product, $key ) {
+  return nextgenthemes\admin\update_key_status( $product, $key );
+}
