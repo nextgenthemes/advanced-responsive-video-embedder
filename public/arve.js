bb9086de9aedb34cf266b75abcbc88cb5b7821b6
Nicolas Jonas <nnico@protonmail.com>
2018-10-19 01:08:44 +0200

    fix bug Gutenberg white space bug

--- public/arve.js
+++ public/arve.js
@@ -5,6 +5,18 @@
 		$('.arve-wrapper').find('p, .video-wrap, .fluid-width-video-wrapper, .fluid-vids').contents().unwrap();
 		$('.arve-wrapper br').remove();
 		$('.arve-iframe, .arve-video').removeAttr('width height style');
+
+		$('.wp-block-embed').each( function( index ) {
+
+			if ( $(this).has('.arve-wrapper') ) {
+
+				$(this).removeClass( 'wp-embed-aspect-16-9 wp-has-aspect-ratio' );
+
+				if ( $(this).has('.wp-block-embed__wrapper') ) {
+					$(this).find('.wp-block-embed__wrapper').contents().unwrap();
+				}
+			}
+		} );
 	};
 
 	function global_id() {
