1f604f2f589b82d617a86e9d7b7b971792b2e2ec
Nicolas Jonas <nnico+gitlab@pm.me>
2019-08-24 01:13:13 +0200

    Pass shortcode args to the embed shortcode for cachebusting.

--- public/EmbedChecker.php
+++ public/EmbedChecker.php
@@ -10,8 +10,9 @@ class EmbedChecker {
 	}
 
 	public function check() {
+
 		add_filter( 'nextgenthemes/arve/oembed2args', [ $this, 'oembed2args' ] );
-		$maybe_arve_html = $GLOBALS['wp_embed']->shortcode( [], $this->shortcode_args['url'] );
+		$maybe_arve_html = $GLOBALS['wp_embed']->shortcode( $this->shortcode_args, $this->shortcode_args['url'] );
 		remove_filter( 'nextgenthemes/arve/oembed2args', [ $this, 'oembed2args' ] );
 
 		if ( false !== strpos( $maybe_arve_html, 'class="arve' ) ) {
