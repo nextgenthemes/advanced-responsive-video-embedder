2f8d709b426ce8020649e70fae72088729426628
Nicolas Jonas <nnico+gitlab@pm.me>
2019-09-16 01:00:02 +0200

    Better remote_get functions

--- php/Common/functions-remote-get.php
+++ php/Common/functions-remote-get.php
@@ -3,16 +3,50 @@ namespace Nextgenthemes\ARVE\Common;
 
 // phpcs:ignore WordPress.WP.I18n.NonSingularStringLiteralDomain
 
-function ngt_remote_get( $url, array $args = [], $json = true ) {
+function ngt_remote_get_json( $url, array $args = [], $json_name = false ) {
 
-	$response      = wp_safe_remote_post( $url, $args );
-	$response_code = wp_remote_retrieve_response_code( $response );
+	$response = ngt_remote_get_body( $url, $args );
 
-	// retry with wp_remote_get
-	if ( is_wp_error( $response ) || 200 !== $response_code ) {
-		$response      = wp_safe_remote_get( $url, $args );
-		$response_code = wp_remote_retrieve_response_code( $response );
+	if ( is_wp_error( $response ) ) {
+		return $response;
 	}
+
+	$response = json_decode( $response );
+
+	if ( null === $response ) {
+		return new \WP_Error(
+			'json-null',
+			sprintf(
+				// Translators: URL.
+				__( 'url: %s json_decode returned null.', 'advanced-responsive-video-embedder' ),
+				esc_url( $url )
+			)
+		);
+	}
+
+	if ( $json_name ) {
+
+		if ( empty( $response->$json_name ) ) {
+			return new \WP_Error(
+				'json-value-empty',
+				sprintf(
+					__( "url: %s JSON value '%s' does not exist or is empty", 'advanced-responsive-video-embedder' ),
+					esc_url( $url ),
+					esc_html( $json_name )
+				)
+			);
+		} else {
+			return $response->$json_name;
+		}
+	}
+
+	return $response;
+}
+
+function ngt_remote_get_body( $url, array $args = [] ) {
+
+	$response      = wp_safe_remote_get( $url, $args );
+	$response_code = wp_remote_retrieve_response_code( $response );
 
 	if ( is_wp_error( $response ) ) {
 		return $response;
@@ -31,9 +65,9 @@ function ngt_remote_get( $url, array $args = [], $json = true ) {
 		);
 	}
 
-	$body = wp_remote_retrieve_body( $response );
+	$response = wp_remote_retrieve_body( $response );
 
-	if ( '' === $body ) {
+	if ( '' === $response ) {
 		return new \WP_Error(
 			'empty-body',
 			sprintf(
@@ -44,28 +78,13 @@ function ngt_remote_get( $url, array $args = [], $json = true ) {
 		);
 	}
 
-	if ( $json ) {
-		$response = json_decode( $body );
-
-		if ( null === $response ) {
-			return new \WP_Error(
-				'json-null',
-				sprintf(
-					// Translators: URL.
-					__( 'url: %s json_decode returned null.', 'advanced-responsive-video-embedder' ),
-					$url
-				)
-			);
-		}
-	}
-
 	return $response;
 };
 
 function ngt_remote_get_cached( $url, array $args, $json = true ) {
 
 	$defaults = array(
-		'cache_time'       => 3600,
+		'cache_time' => 3600,
 	);
 
 	$args = wp_parse_args( $args, $defaults );
