30ddd93e13c092f98daa9d676dd758202c756cf5
Nicolas Jonas <nnico+gitlab@pm.me>
2019-11-01 01:16:00 +0100

    fix plugin updater

--- php/Common/init.php
+++ php/Common/init.php
@@ -16,7 +16,6 @@ require_once __DIR__ . '/Admin/EDD/PluginUpdater.php';
 require_once __DIR__ . '/Admin/EDD/ThemeUpdater.php';
 require_once __DIR__ . '/Admin/NoticeFactory.php';
 require_once __DIR__ . '/Admin/functions-licensing.php';
-require_once __DIR__ . '/Admin/functions-licensing.php';
 require_once __DIR__ . '/Admin/functions-settings.php';
 require_once __DIR__ . '/Admin/functions-notices.php';
 // phpcs:enable WPThemeReview.CoreFunctionality.FileInclude.FileIncludeFound
