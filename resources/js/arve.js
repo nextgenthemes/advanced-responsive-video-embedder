f909b866f7e217eaedafebb825cb778f62b56c28
Nicolas Jonas <nnico+gitlab@pm.me>
2020-05-28 20:14:52 +0200

    formatting mostly

--- resources/js/arve.js
+++ resources/js/arve.js
@@ -1,88 +1,89 @@
-const qsa = document.querySelectorAll.bind( document );
+const qsa = document.querySelectorAll.bind(document);
 
-document.body.addEventListener( 'mouseover', iframeEnterEvent, false );
-document.body.addEventListener( 'touchend', iframeEnterEvent, false );
+document.body.addEventListener('mouseover', iframeEnterEvent, false);
+document.body.addEventListener('touchend', iframeEnterEvent, false);
 
-function iframeEnterEvent( ev ) {
+function iframeEnterEvent(ev) {
+	const ele = ev.target;
+	const arve = ele.closest('.arve[mode="normal"]');
 
-	const ele  = ev.target;
-	const arve = ele.closest( '.arve[mode="normal"]' );
-
-	if ( ele && ele.matches( 'iframe' ) && arve ) {
-
-		rmClass( 'arve--clicked', 'arve--clicked' );
-		arve.classList.add( 'arve--clicked' );
+	if (ele && ele.matches('iframe') && arve) {
+		rmClass('arve--clicked', 'arve--clicked');
+		arve.classList.add('arve--clicked');
 	}
 }
 
-document.body.addEventListener( 'play', videoPlayEvent, true );
+document.body.addEventListener('play', videoPlayEvent, true);
 
-function videoPlayEvent( ev ) {
+function videoPlayEvent(ev) {
+	const ele = ev.target;
+	const arve = ele.closest('.arve');
 
-	const ele  = ev.target;
-	const arve = ele.closest( '.arve' );
-
-	if ( ele && ele.matches( 'video' ) && arve ) {
-		rmClass( 'arve--clicked', 'arve--clicked' );
-		arve.classList.add( 'arve--clicked' );
+	if (ele && ele.matches('video') && arve) {
+		rmClass('arve--clicked', 'arve--clicked');
+		arve.classList.add('arve--clicked');
 	}
 }
 
-function rmClass( selector, ...classes ) {
-	qsa( selector ).forEach( ( el ) => {
-		el.classList.remove( ...classes );
-	} );
+function rmClass(selector, ...classes) {
+	qsa(selector).forEach((el) => {
+		el.classList.remove(...classes);
+	});
 }
 
-function unwrap( el ) {
+function unwrap(el) {
 	// get the element's parent node
 	const parent = el.parentNode;
 	// move all children out of the element
-	while ( el.firstChild ) {
-		parent.insertBefore( el.firstChild, el );
+	while (el.firstChild) {
+		parent.insertBefore(el.firstChild, el);
 	}
 	// remove the empty element
-	parent.removeChild( el );
+	parent.removeChild(el);
 }
 
 function removeUnwantedStuff() {
-	qsa( '.arve p, .arve .video-wrap, .arve .fluid-width-video-wrapper, .arve .fluid-vids' ).forEach( ( el ) => {
-		unwrap( el );
-	} );
+	qsa(
+		'.arve p, .arve .video-wrap, .arve .fluid-width-video-wrapper, .arve .fluid-vids'
+	).forEach((el) => {
+		unwrap(el);
+	});
 
-	qsa( '.arve br' ).forEach( ( el ) => {
+	qsa('.arve br').forEach((el) => {
 		el.remove();
-	} );
+	});
 
-	qsa( '.arve-iframe, .arve-video' ).forEach( ( el ) => {
-		el.removeAttribute( 'width' );
-		el.removeAttribute( 'height' );
-		el.removeAttribute( 'style' );
-	} );
+	qsa('.arve-iframe, .arve-video').forEach((el) => {
+		el.removeAttribute('width');
+		el.removeAttribute('height');
+		el.removeAttribute('style');
+	});
 
-	qsa( '.wp-block-embed' ).forEach( ( el ) => {
-		if ( el.querySelector( '.arve' ) ) {
-			el.classList.remove( [ 'wp-embed-aspect-16-9', 'wp-has-aspect-ratio' ] );
+	qsa('.wp-block-embed').forEach((el) => {
+		if (el.querySelector('.arve')) {
+			el.classList.remove([
+				'wp-embed-aspect-16-9',
+				'wp-has-aspect-ratio',
+			]);
 
-			const $WRAPPER = el.querySelector( '.wp-block-embed__wrapper' );
+			const $WRAPPER = el.querySelector('.wp-block-embed__wrapper');
 
-			if ( $WRAPPER ) {
-				unwrap( $WRAPPER );
+			if ($WRAPPER) {
+				unwrap($WRAPPER);
 			}
 		}
-	} );
+	});
 }
 
 function globalID() {
-
 	// Usually the id should be already there added with php using the language_attributes filter
-	if ( 'global' === document.documentElement.id ) {
+	if ('global' === document.documentElement.id) {
 		return;
 	}
 
-	if ( ! document.documentElement.id ) {
+	if (!document.documentElement.id) {
 		document.documentElement.id = 'global';
-	} else if ( ! document.body.id ) {
+	} else if (!document.body.id) {
 		document.body.id = 'global';
 	}
 }
@@ -90,7 +91,7 @@ function globalID() {
 removeUnwantedStuff();
 globalID();
 
-document.addEventListener( 'DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
 	removeUnwantedStuff();
 	globalID();
-} );
+});
