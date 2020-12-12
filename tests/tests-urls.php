e8515fcac34f853328f457ebe6ec250f83c501a5
Nicolas Jonas <nnico+gitlab@pm.me>
2020-12-12 17:58:09 +0100

    switch test to vimeo because youtube fails on github CI

--- tests/tests-urls.php
+++ tests/tests-urls.php
@@ -5,9 +5,9 @@ class Tests_URLs extends WP_UnitTestCase {
 
 	public function test_urls() {
 
-		$output = apply_filters( 'the_content', 'https://www.youtube.com/watch?v=2an6-WVPuJU' );
+		$output = apply_filters( 'the_content', 'https://vimeo.com/265932452' );
 
 		$this->assertNotContains( 'Error', $output );
-		$this->assertContains( 'src="https://www.youtube-nocookie.com/embed/2an6-WVPuJU', $output );
+		$this->assertContains( 'src="https://player.vimeo.com/video/265932452', $output );
 	}
 }
