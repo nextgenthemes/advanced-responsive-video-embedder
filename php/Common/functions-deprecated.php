3d6e4593c7b9884190e0f8302f7b622dd930a92f
Nicolas Jonas <nnico+gitlab@pm.me>
2021-01-15 17:30:15 +0100

    add deprecated functions

---
+++ php/Common/functions-deprecated.php
@@ -0,0 +1,13 @@
+<?php
+
+function contains( $haystack, $needle ) {
+	return str_contains( $haystack, $needle );
+}
+
+function starts_with( $haystack, $needle ) {
+	return str_starts_with( $haystack, $needle );
+}
+
+function ends_with( $haystack, $needle ) {
+	return str_ends_with( $haystack, $needle );
+}
--- php/Common/init.php
+++ php/Common/init.php
@@ -5,7 +5,8 @@ const VERSION = '1.0.0';
 
 // phpcs:disable WPThemeReview.CoreFunctionality.FileInclude.FileIncludeFound
 require_once __DIR__ . '/functions-compat.php';
-
+require_once __DIR__ . '/functions-deprecated.php';
+// ------------------------------------------------
 require_once __DIR__ . '/Settings.php';
 require_once __DIR__ . '/functions-settings.php';
 require_once __DIR__ . '/functions-license.php';
