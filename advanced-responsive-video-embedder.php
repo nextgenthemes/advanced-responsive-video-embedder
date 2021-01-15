20ca6d1111587cc14173e07ff0b2d9a34084407b
Nicolas Jonas <nnico+gitlab@pm.me>
2021-01-15 17:32:41 +0100

    Update version tags to 9.2.0-alpha4

--- advanced-responsive-video-embedder.php
+++ advanced-responsive-video-embedder.php
@@ -3,7 +3,7 @@
  * Plugin Name:       ARVE Advanced Responsive Video Embedder
  * Plugin URI:        https://nextgenthemes.com/plugins/arve-pro/
  * Description:       Easy responsive video embeds via URL (like WordPress) or Shortcodes. Supports almost anything you can imagine.
- * Version:           9.2.0-alpha3
+ * Version:           9.2.0-alpha4
  * Author:            Nicolas Jonas
  * Author URI:        https://nextgenthemes.com
  * License:           GPL-3.0
@@ -21,7 +21,7 @@
 
 namespace Nextgenthemes\ARVE;
 
-const VERSION               = '9.2.0-alpha3';
+const VERSION               = '9.2.0-alpha4';
 const PRO_VERSION_REQUIRED  = '5.0.0-beta5';
 const NUM_TRACKS            = 3;
 const PLUGIN_FILE           = __FILE__;
