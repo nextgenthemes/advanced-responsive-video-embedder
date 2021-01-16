d5a4c1238f4ff6a01425143fe5d05a78cf982019
Nicolas Jonas <nnico+gitlab@pm.me>
2021-01-16 07:34:57 +0100

    test [video] sc with loop

---
+++ tests/tests-sc-native.php
@@ -0,0 +1,21 @@
+<?php
+use function Nextgenthemes\ARVE\shortcode;
+// phpcs:disable Squiz.PHP.CommentedOutCode.Found
+// phpcs:disable Squiz.Classes.ClassFileName.NoMatch
+// phpcs:disable Squiz.PHP.Classes.ValidClassName.NotCamelCaps
+class Tests_Embed_Shortcode extends WP_UnitTestCase {
+
+	public function test_embed_sc_mp4() {
+
+		$html = apply_filters( 'the_content', '[embed]https://www.example.com/wp-content/uploads/2020/05/sample-mp4-file.mp4[/embed]' );
+		$this->assertContains( 'class="arve-video', $html );
+		$this->assertNotContains( 'Error', $html );
+	}
+
+	public function test_video_sc_mp4() {
+
+		$html = apply_filters( 'the_content', '[video src="https://www.example.com/wp-content/uploads/2020/05/sample-mp4-file.mp4" loop="on" /]' );
+		$this->assertContains( 'class="arve-video', $html );
+		$this->assertNotContains( 'Error', $html );
+	}
+}
