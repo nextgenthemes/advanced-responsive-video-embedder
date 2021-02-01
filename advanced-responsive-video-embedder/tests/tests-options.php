a33de725b631c1678a3ed665b59e810b0838fe40
Nicolas Jonas <nnico+git@pm.me>
2021-02-01 23:09:32 +0100

    basic addon checks, rename

--- advanced-responsive-video-embedder/tests/tests-options.php
+++ advanced-responsive-video-embedder/tests/tests-options.php
@@ -1,6 +1,6 @@
 <?php
 // phpcs:disable Squiz.PHP.CommentedOutCode.Found, Squiz.Classes.ClassFileName.NoMatch, Squiz.PHP.Classes.ValidClassName.NotCamelCaps, WordPress.PHP.DevelopmentFunctions.error_log_print_r, WordPress.PHP.DevelopmentFunctions.error_log_error_log
-class Tests_Options extends WP_UnitTestCase {
+class Tests_ARVEProOptions extends WP_UnitTestCase {
 
 	public function test_options_v8_to_v9() {
 
