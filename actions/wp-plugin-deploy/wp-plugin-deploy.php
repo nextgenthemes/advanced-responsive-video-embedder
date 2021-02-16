5229e3148f2b92a8feca6a61656341057a4668d8
Nicolas Jonas <nnico+git@pm.me>
2021-02-16 12:29:02 +0100

    try new action

---
+++ actions/wp-plugin-deploy/wp-plugin-deploy.php
@@ -0,0 +1,121 @@
+#!/usr/bin/env php
+<?php
+use function \escapeshellarg as e;
+
+require_once 'wp-plugin-deploy-functions.php';
+
+$workdir = arg_with_default('workdir', false);
+
+if ( $workdir ) {
+	chdir("./$workdir");
+}
+
+$slug        = basename(getcwd());
+$dir         = getcwd();
+$git_dir     = sys('git rev-parse --show-toplevel');
+$subdir      = trim(str_replace($git_dir, '', $dir), '/');
+$svn_user    = arg_with_default('svn-user', false);
+$svn_pass    = arg_with_default('svn-pass', false);
+$build_dirs  = arg_with_default('build-dirs', false);
+$svn_url     = "https://plugins.svn.wordpress.org/$slug/";
+$tmp_dir     = '/tmp/wp-deploy';
+$svn_dir     = "$tmp_dir/svn-$slug";
+$gitarch_dir = "$tmp_dir/gitarchive-$slug";
+$readme_only = has_arg('readme-only');
+$dry_run     = has_arg('dry-run');
+
+if ( $readme_only ) {
+	$commit_msg = 'Update readme and assets from current dir with automation script';
+} else {
+	$version    = required_arg('version');
+	$commit_msg = "Update to $version with automation script";
+}
+
+if ( $build_dirs ) {
+	$build_dirs = explode(',', $build_dirs);
+}
+
+sys('rm -rf ' . e($tmp_dir) );
+
+var_export(
+	compact(
+		'slug',
+		'dir',
+		'git_dir',
+		'subdir',
+		'svn_url',
+		'build_dirs',
+		'tmp_dir',
+		'version'
+	)
+);
+
+# Checkout just trunk and assets for efficiency
+# Tagging will be handled on the SVN level
+echo '➤ Checking out wp.org repository...';
+sys( 'svn checkout --depth immediates ' . e($svn_url) . ' ' . e($svn_dir) );
+
+chdir($svn_dir);
+sys('svn update --set-depth infinity assets');
+sys('svn update --set-depth infinity trunk');
+
+echo '➤ Copying files...' . PHP_EOL;
+
+if ( $readme_only ) {
+	copy( "$dir/readme.txt", "$svn_dir/trunk/readme.txt" );
+	copy( "$dir/readme.txt", "$svn_dir/tags/$last_svg_tag/readme.txt" );
+} else {
+	mkdir($gitarch_dir);
+	sys('git --git-dir='.e("$git_dir/.git").' archive '.e("$version:$subdir").' | tar x --directory='.e($gitarch_dir));
+	sys('rsync -rc '.e("$gitarch_dir/").' '.e("$svn_dir/trunk").' --delete --delete-excluded');
+
+	foreach ( $build_dirs as $build_dir ) {
+		$build_dir = trim( $build_dir );
+
+		if ( ! file_exists( "$dir/$build_dir" ) ) {
+			echo "Build dir $dir/$build_dir does not exists." . PHP_EOL;
+			exit(1);
+		}
+
+		sys('rsync -rc '.e("$dir/$build_dir").' '.e("$svn_dir/trunk/").' --delete');
+	}
+}
+
+sys('rsync -rc '.e("$dir/.wordpress-org/").' '.e("$svn_dir/assets").' --delete');
+
+# Add everything and commit to SVN
+# The force flag ensures we recurse into subdirectories even if they are already added
+echo '➤ Preparing files...' . PHP_EOL;
+sys('svn add . --force --quiet');
+
+# SVN delete all deleted files
+# Also suppress stdout here
+sys("svn status | grep '^\!' | sed 's/! *//' | xargs -I% svn rm %@ --quiet");
+
+# Copy tag locally to make this a single commit
+if ( ! $readme_only ) {
+	echo '➤ Copying tag...' . PHP_EOL;
+	sys('svn cp trunk '.e("tags/$version"));
+}
+
+# Fix screenshots getting force downloaded when clicking them
+# https://developer.wordpress.org/plugins/wordpress-org/plugin-assets/
+system('svn propset svn:mime-type image/png assets/*.png');
+system('svn propset svn:mime-type image/jpeg assets/*.jpg');
+
+sys('svn status');
+
+echo '➤ Committing files...' . PHP_EOL;
+$commit_command = 'svn commit -m '.e($commit_msg);
+
+if ( $dry_run ) {
+	$commit_command .= ' --dry-run';
+}
+
+if ( $svn_user && $svn_pass ) {
+	$commit_command .= ' --no-auth-cache --non-interactive --username '.e($svn_user). ' --password '.e($svn_pass);
+}
+
+sys($commit_command);
+
+echo '✓ Plugin deployed!';
