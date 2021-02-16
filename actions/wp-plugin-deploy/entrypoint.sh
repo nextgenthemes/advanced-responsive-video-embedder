17d99a5f05ef92b501d116127b5b78c197972ec9
Nicolas Jonas <nnico+git@pm.me>
2021-02-16 13:34:39 +0100

    ...

--- actions/wp-plugin-deploy/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# `$*` expands the `args` supplied in an `array` individually
-# or splits `args` in a string separated by whitespace.
-sh -c "echo $*"
-
-sh -c "./wp-plugin-deploy.php $*"
--- actions/wp-plugin-deploy/wp-plugin-deploy.php
+++ actions/wp-plugin-deploy/wp-plugin-deploy.php
@@ -119,3 +119,51 @@ if ( $svn_user && $svn_pass ) {
 sys($commit_command);
 
 echo '✓ Plugin deployed!';
+
+function has_arg( string $arg ): bool {
+	$getopt = getopt( null, [ $arg ] );
+	return isset($getopt[ $arg ]);
+}
+
+function required_arg( string $arg ): string {
+
+	$getopt = getopt( null, [ "$arg:" ] );
+
+	if ( empty($getopt[ $arg ]) ) {
+		echo "need --$arg=x";
+		exit(1);
+	}
+
+	return $getopt[ $arg ];
+}
+
+function arg_with_default( string $arg, $default ): string {
+
+	$getopt = getopt( null, [ "$arg::" ] );
+
+	if ( empty($getopt[ $arg ]) ) {
+		return $default;
+	}
+
+	return $getopt[ $arg ];
+}
+
+function sys( string $command, array $args = [] ): ?string {
+
+	foreach ( $args as $k => $v ) {
+		$command .= " --$k=" . escapeshellarg($v);
+	}
+
+	echo "Executing: $command" . PHP_EOL;
+
+	$out = system( $command, $exit_code );
+
+	if ( 0 !== $exit_code || false === $out ) {
+		echo 'Error, output: ';
+		var_dump($out);
+		echo "Exit Code: $exit_code." . PHP_EOL;
+		exit($exit_code);
+	}
+
+	return $out;
+}
