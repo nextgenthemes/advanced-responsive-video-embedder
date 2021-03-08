052cc9314a9bf496937a30476e5ccaf428dad188
Nicolas Jonas <nnico+git@pm.me>
2021-03-08 10:24:03 +0100

    try this [ci]

--- plugins/advanced-responsive-video-embedder/tests/tests-sc-thumbnail.php
+++ plugins/advanced-responsive-video-embedder/tests/tests-sc-thumbnail.php
@@ -8,14 +8,14 @@ class Tests_ShortcodeThumbnail extends WP_UnitTestCase {
 	public function test_thumbnails() {
 
 		$filename = dirname( __FILE__ ) . '/test-attachment.jpg';
-		// phpcs:disable WordPress.WP.AlternativeFunctions.file_get_contents_file_get_contents
+		// phpcs:ignore WordPress.WP.AlternativeFunctions.file_get_contents_file_get_contents
 		$contents = file_get_contents( $filename );
-		// phpcs:enable WordPress.WP.AlternativeFunctions.file_get_contents_file_get_contents
 
 		$upload = wp_upload_bits( basename( $filename ), null, $contents );
 		$this->assertTrue( empty( $upload['error'] ) );
 
-		$attachment_id = parent::_make_attachment( $upload );
+		// phpcs:ignore WordPress.PHP.NoSilencedErrors.Discouraged
+		$attachment_id = @parent::_make_attachment( $upload );
 
 		$attr = array(
 			'url'       => 'https://example.com/video.mp4',
