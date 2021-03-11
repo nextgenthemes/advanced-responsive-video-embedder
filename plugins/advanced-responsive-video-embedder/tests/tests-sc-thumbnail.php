3c919a608afffe163ecf7b9ec63a259628cabd46
Nicolas Jonas <nnico+git@pm.me>
2021-03-11 02:11:24 +0100

    try different img [ci]

--- plugins/advanced-responsive-video-embedder/tests/tests-sc-thumbnail.php
+++ plugins/advanced-responsive-video-embedder/tests/tests-sc-thumbnail.php
@@ -2,22 +2,17 @@
 use function \Nextgenthemes\ARVE\shortcode;
 use function \Nextgenthemes\ARVE\get_host_properties;
 
-// phpcs:disable Squiz.PHP.CommentedOutCode.Found, Squiz.Classes.ClassFileName.NoMatch, Squiz.PHP.Classes.ValidClassName.NotCamelCaps, WordPress.PHP.DevelopmentFunctions.error_log_print_r, WordPress.PHP.DevelopmentFunctions.error_log_error_log
+// phpcs:disable Squiz.PHP.CommentedOutCode.Found, Squiz.Classes.ClassFileName.NoMatch, Squiz.PHP.Classes.ValidClassName.NotCamelCaps, WordPress.PHP.DevelopmentFunctions.error_log_print_r, WordPress.PHP.DevelopmentFunctions.error_log_error_log, WordPress.WP.AlternativeFunctions.file_get_contents_file_get_contents
 class Tests_ShortcodeThumbnail extends WP_UnitTestCase {
 
 	public function test_thumbnails() {
-		if ( version_compare( $GLOBALS['wp_version'], '5.7', '>=' ) ) {
-			$this->expectException('Exception');
-		}
-
-		$filename = dirname( __FILE__ ) . '/test-attachment.jpg';
-		// phpcs:ignore WordPress.WP.AlternativeFunctions.file_get_contents_file_get_contents
+		$filename = \Nextgenthemes\ARVE\PLUGIN_DIR . '/.wordpress-org/icon-128x128.png';
 		$contents = file_get_contents( $filename );
 
 		$upload = wp_upload_bits( basename( $filename ), null, $contents );
 		$this->assertTrue( empty( $upload['error'] ) );
+		pd('test');
 
-		// phpcs:ignore WordPress.PHP.NoSilencedErrors.Discouraged
 		$attachment_id = parent::_make_attachment( $upload );
 
 		$attr = array(
@@ -26,7 +21,7 @@ class Tests_ShortcodeThumbnail extends WP_UnitTestCase {
 			'title'     => 'Something',
 		);
 
-		$this->assertRegExp( '#<meta itemprop="thumbnailUrl" content=".*test-attachment#', shortcode( $attr ) );
+		$this->assertRegExp( '#<meta itemprop="thumbnailUrl" content=".*icon-128x128.png#', shortcode( $attr ) );
 
 		$attr = array(
 			'url'       => 'https://example.com/video2.mp4',
