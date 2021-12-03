617cfd470a0e7d3285bcb386d6f83ad8cb061649
Nicolas Jonas <nnico+git@pm.me>
2021-12-03 18:47:55 +0100

    update test code

--- plugins/advanced-responsive-video-embedder/tests/bootstrap.php
+++ plugins/advanced-responsive-video-embedder/tests/bootstrap.php
@@ -1,28 +1,44 @@
 <?php
-
-// phpcs:disable WordPress.WP.GlobalVariablesOverride.Prohibited
-$_SERVER['SERVER_PROTOCOL'] = 'HTTP/1.1';
-$_SERVER['SERVER_NAME']     = '';
-$PHP_SELF                   = '/index.php';
-$GLOBALS['PHP_SELF']        = '/index.php';
-$_SERVER['PHP_SELF']        = '/index.php';
-
+/**
+ * PHPUnit bootstrap file.
+ *
+ * @package Scuff
+ */
 require_once dirname( dirname( __FILE__ ) ) . '/vendor/autoload.php';
 
+
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
+
 if ( ! $_tests_dir ) {
-	$_tests_dir = '/tmp/wordpress-tests-lib';
+	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
 }
 
-require_once $_tests_dir . '/includes/functions.php';
+// Forward custom PHPUnit Polyfills configuration to PHPUnit bootstrap file.
+$_phpunit_polyfills_path = getenv( 'WP_TESTS_PHPUNIT_POLYFILLS_PATH' );
+if ( false !== $_phpunit_polyfills_path ) {
+	define( 'WP_TESTS_PHPUNIT_POLYFILLS_PATH', $_phpunit_polyfills_path );
+}
 
-function ci_manually_load_plugin() {
+if ( ! file_exists( "{$_tests_dir}/includes/functions.php" ) ) {
+	echo "Could not find {$_tests_dir}/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+	exit( 1 );
+}
+
+// Give access to tests_add_filter() function.
+require_once "{$_tests_dir}/includes/functions.php";
+
+/**
+ * Manually load the plugin being tested.
+ */
+function _manually_load_plugin() {
 	# /home/travis/build/nextgenthemes/advanced-responsive-video-embedder
 	require dirname( __DIR__ ) . '/advanced-responsive-video-embedder.php';
 }
-tests_add_filter( 'muplugins_loaded', 'ci_manually_load_plugin' );
 
-require $_tests_dir . '/includes/bootstrap.php';
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+// Start up the WP testing environment.
+require "{$_tests_dir}/includes/bootstrap.php";
 
 activate_plugin( 'advanced-responsive-video-embedder/advanced-responsive-video-embedder.php' );
 
