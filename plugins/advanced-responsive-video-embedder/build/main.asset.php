67b5369f17f9d5cb5c1bd2613d7ede4f550df7d8
Nicolas Jonas <nnico+git@pm.me>
2022-04-16 04:56:29 +0200

    npm run build

--- plugins/advanced-responsive-video-embedder/build/main.asset.php
+++ plugins/advanced-responsive-video-embedder/build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '6619c5dd05c685e8b74e3b4390ed9904');
+<?php return array('dependencies' => array(), 'version' => '1bca356de819b25e2c48f05fc0d1b1e9');
