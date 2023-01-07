fe46eaf58bcddc4f3b6cb8eba5366c8aa116036d
Nicolas Jonas <nnico+git@pm.me>
2023-01-07 16:15:40 +0100

    npm run build

--- plugins/advanced-responsive-video-embedder/build/block.asset.php
+++ plugins/advanced-responsive-video-embedder/build/block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '69dc5fa1c5fd02603ecf');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '82cb6e0113f09e1ed792');
