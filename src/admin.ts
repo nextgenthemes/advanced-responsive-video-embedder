93af2c3111364eff187da1dd99c94fd0d209b199
Nicolas Jonas <nnico+git@pm.me>
2023-03-25 08:23:55 +0100

    convert partially to vanilla  js

--- src/admin.ts
+++ src/admin.ts
@@ -11,21 +11,36 @@ declare global {
 	}
 }
 
-window.jQuery( document ).ready( function ( $ ) {
-	$( '.notice.is-dismissible' ).on(
-		'click',
-		'.notice-dismiss',
-		function ( event ) {
-			event.preventDefault();
-			const $this = $( this );
-			if ( 'undefined' == $this.parent().attr( 'id' ) ) {
-				return;
-			}
-			$.post( window.ajaxurl, {
-				action: 'dnh_dismiss_notice',
-				url: window.ajaxurl,
-				id: $this.parent().attr( 'id' ),
-			} );
+document.body.addEventListener( 'click', ( event ) => {
+	const target = event?.target as HTMLElement | null;
+
+	if ( target && target.matches( '.notice-dismiss' ) ) {
+		event.preventDefault();
+
+		const parent = target.parentNode as HTMLDivElement | null;
+		const noticeId = parent?.getAttribute( 'id' );
+
+		if ( ! parent?.matches( '.notice.is-dismissible' ) || ! noticeId ) {
+			return;
 		}
-	);
+
+		window.jQuery.post( window.ajaxurl, {
+			action: 'dnh_dismiss_notice',
+			url: window.ajaxurl,
+			id: noticeId,
+		} );
+
+		// does not work
+		// fetch( window.ajaxurl, {
+		// 	method: 'POST',
+		// 	headers: {
+		// 		'Content-Type': 'application/json',
+		// 	},
+		// 	body: JSON.stringify( {
+		// 		action: 'dnh_dismiss_notice',
+		// 		url: window.ajaxurl,
+		// 		id: noticeId,
+		// 	} ),
+		// } );
+	}
 } );
