c34c74953dd1a7736590610f5988e3646a79ffce
Nicolas Jonas <nnico+git@pm.me>
2023-04-09 02:42:15 +0200

    fix message

--- php/Admin/functions-admin.php
+++ php/Admin/functions-admin.php
@@ -69,13 +69,13 @@ function action_admin_init_setup_messages() {
 
 	if ( PHP_VERSION_ID < 70200 ) {
 		$msg = esc_html__(
-			'Your php version is very outdated, the next version of ARVE will require probably require php 7.2+, possibly 7.2+ please update (ask your host to update).',
+			'Your php version is very outdated, the next version of ARVE will probably require php 7.2+, possibly 7.4+ please update (ask your host to update).',
 			'advanced-responsive-video-embedder'
 		);
 
 		Notices::instance()->register_notice(
-			'ngt-arve-addon-ad',
-			'notice-info',
+			'arve-php-outdated-warn',
+			'notice-error',
 			$msg
 		);
 	}
