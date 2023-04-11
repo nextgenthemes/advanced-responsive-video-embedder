ec6dc0a6f268aa40e7dd2c04a5096d7ed75831d3
Nicolas Jonas <nnico+git@pm.me>
2023-04-11 20:01:24 +0200

    phpcbf

--- build/block.asset.php
+++ build/block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '4ce4c4c17e973e30ea94');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '2ba980564ff87ac743fa');
