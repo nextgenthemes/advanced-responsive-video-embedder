7066a0441ae01f69712646cff289666838d8c2dc
Nicolas Jonas <nnico+git@pm.me>
2023-09-16 00:29:56 +0200

    build

--- build/block.asset.php
+++ build/block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '6c91dd3efe632dffbb7e');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '72980df0ca8d024672d7');
