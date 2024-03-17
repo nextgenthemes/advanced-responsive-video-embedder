a09da40ac5bf8ef0e0e0243cbfd938255ec3422c
Nicolas Jonas <nnico+git@pm.me>
2024-03-17 17:58:43 +0100

    Make this usable outside WP

--- vendor/nextgenthemes/wp-shared/includes/WP/Admin/load-admin-files.php
+++ vendor/nextgenthemes/wp-shared/includes/WP/Admin/load-admin-files.php
@@ -1,7 +1,7 @@
 <?php declare(strict_types=1);
 namespace Nextgenthemes\WP;
 
-if ( is_admin() ) {
+if ( function_exists( '\is_admin' ) && \is_admin() ) {
 	require_once __DIR__ . '/EDD/PluginUpdater.php';
 	require_once __DIR__ . '/EDD/ThemeUpdater.php';
 	require_once __DIR__ . '/Notices.php';
