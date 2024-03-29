9bfa06e98e0964019b168b51da36ef47a9ebccc7
Nicolas Jonas <nnico+git@pm.me>
2024-03-29 19:23:54 +0100

    iframe extraction

--- src/shortcode-dialog.js
+++ src/shortcode-dialog.js
@@ -2,11 +2,58 @@
 
 document.addEventListener( 'alpine:init', () => {
 	const dialog = document.querySelector( '.arve-sc-dialog' );
-	const helpTexts = dialog?.querySelectorAll( '.arve-sc-dialog__description' );
 
-	if ( ! dialog ) {
+	if ( ! ( dialog instanceof HTMLDialogElement ) ) {
 		return;
 	}
+
+	const helpTexts = dialog.querySelectorAll( '.arve-sc-dialog__description' );
+	const domParser = new DOMParser();
+
+	/**
+	 * Calculate aspect ratio based on width and height.
+	 *
+	 * @param {string} width  - The width value
+	 * @param {string} height - The height value
+	 * @return {string} The aspect ratio in the format 'width:height'
+	 */
+	const aspectRatio = ( width, height ) => {
+		if ( isIntOverZero( width ) && isIntOverZero( height ) ) {
+			const w = parseInt( width );
+			const h = parseInt( height );
+			const arGCD = gcd( w, h );
+
+			return w / arGCD + ':' + h / arGCD;
+		}
+
+		return width + ':' + height;
+	};
+
+	/**
+	 * Checks if the input string represents a positive integer.
+	 *
+	 * @param {string} str - the input string to be checked
+	 * @return {boolean} true if the input string represents a positive integer, false otherwise
+	 */
+	const isIntOverZero = ( str ) => {
+		const n = Math.floor( Number( str ) );
+		return n !== Infinity && String( n ) === str && n > 0;
+	};
+
+	/**
+	 * Calculate the greatest common divisor of two numbers using the Euclidean algorithm.
+	 *
+	 * @param {number} a - the first number
+	 * @param {number} b - the second number
+	 * @return {number} the greatest common divisor of the two input numbers
+	 */
+	const gcd = ( a, b ) => {
+		if ( ! b ) {
+			return a;
+		}
+
+		return gcd( b, a % b );
+	};
 
 	// @ts-ignore
 	Alpine.data( 'arvedialog', () => ( {
@@ -18,6 +65,24 @@ document.addEventListener( 'alpine:init', () => {
 			let out = '';
 
 			for ( const [ key, value ] of Object.entries( this.options ) ) {
+				if ( 'url' === key ) {
+					const iframe = domParser
+						.parseFromString( value, 'text/html' )
+						.querySelector( 'iframe' );
+					if ( iframe && iframe.getAttribute( 'src' ) ) {
+						this.options.url = iframe.getAttribute( 'src' );
+
+						if ( iframe.width && iframe.height ) {
+							const ratio = aspectRatio( iframe.width, iframe.height );
+
+							if ( '16:9' !== ratio ) {
+								this.options.aspect_ratio = ratio;
+							}
+						}
+						return;
+					}
+				}
+
 				if ( true === value ) {
 					out += `${ key }="true" `;
 				}
