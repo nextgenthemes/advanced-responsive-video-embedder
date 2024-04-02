becd8d3daa404c65506bcc44cf3f067e9f0af9ac
Nicolas Jonas <nnico+git@pm.me>
2024-04-02 19:52:54 +0200

    fix test

--- tests/tests-settings.php
+++ tests/tests-settings.php
@@ -1,18 +1,9 @@
 <?php
-
 use Nextgenthemes\ARVE;
-use Nextgenthemes\ARVE\Admin;
 
-
-// phpcs:disable Squiz.Classes.ClassFileName.NoMatch
-// phpcs:disable Squiz.Classes.ValidClassName.NotCamelCaps
 class Tests_Settings extends WP_UnitTestCase {
 
 	public function test_settings_page(): void {
-
-		$i       = ARVE\settings_instance();
-		$options = $i->get_options();
-
-		$this->assertArrayHasKey( 'maxwidth', $options );
+		$this->assertArrayHasKey( 'maxwidth', ARVE\options() );
 	}
 }
