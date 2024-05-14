401ca73743015d15fd0cf9810799041ad9bd2267
Nicolas Jonas <nnico+git@pm.me>
2024-05-15 00:15:05 +0200

    allow referrer test

--- tests/tests-sc-vimeo.php
+++ tests/tests-sc-vimeo.php
@@ -1,8 +1,6 @@
 <?php
 use function Nextgenthemes\ARVE\shortcode;
-use function Nextgenthemes\ARVE\get_host_properties;
 
-// phpcs:disable Squiz.PHP.CommentedOutCode.Found, Squiz.Classes.ClassFileName.NoMatch, Squiz.PHP.Classes.ValidClassName.NotCamelCaps, WordPress.PHP.DevelopmentFunctions.error_log_print_r, WordPress.PHP.DevelopmentFunctions.error_log_error_log
 class Tests_ShortcodeVimeo extends WP_UnitTestCase {
 
 	public function test_vimeo_time_and_sandbox(): void {
@@ -14,6 +12,7 @@ class Tests_ShortcodeVimeo extends WP_UnitTestCase {
 		);
 
 		$this->assertStringNotContainsString( 'Error', $html );
+		$this->assertStringNotContainsString( 'referrerpolicy', $html );
 		$this->assertMatchesRegularExpression( '@src="https://player.vimeo.com/.*#t=33"@', $html );
 		$this->assertStringContainsString( 'allow-forms', $html );
 	}
