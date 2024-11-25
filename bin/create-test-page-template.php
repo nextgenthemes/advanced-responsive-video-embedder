3b03384fe4c3155a2b2d363dae81b3e221a8f9c5
Nicolas Jonas <nnico+git@pm.me>
2024-11-26 00:14:36 +0100

    script for creating and updating test pages

---
+++ bin/create-test-page-template.php
@@ -0,0 +1,9 @@
+<!-- wp:code {"align":"wide"} -->
+<pre class="wp-block-code alignwide"><code>&#91;arve url="<?= $url; ?>" mode="normal" /]
+&#91;arve url="<?= $url; ?>" mode="lazyload" /]</code></pre>
+<!-- /wp:code -->
+
+<!-- wp:shortcode -->
+[arve url="<?= $url; ?>" mode="normal" /]
+[arve url="<?= $url; ?>" mode="lazyload" /]
+<!-- /wp:shortcode -->
