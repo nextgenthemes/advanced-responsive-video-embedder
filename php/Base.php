e65322b65448684799a92c7e9b764a4b41edf6b2
Nicolas Jonas <nnico+git@pm.me>
2024-12-06 17:01:35 +0100

    get settings from setting class

--- php/Base.php
+++ php/Base.php
@@ -1,4 +1,7 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types = 1);
+
 namespace Nextgenthemes\ARVE;
 
 use WP_Error;
@@ -9,16 +12,12 @@ class Base {
 	private Settings $settings_instance;
 	private \WP_Error $errors;
 
-	/** @readonly */
-	private array $settings_data;
-
 	public function __construct() {
-		$this->settings_data     = settings_data();
 		$this->errors            = new \WP_Error();
 		$this->settings_instance = new Settings(
 			array(
 				'namespace'           => __NAMESPACE__,
-				'settings'            => settings( 'settings_page', $this->settings_data ),
+				'settings'            => settings( 'settings_page', settings_data() ),
 				'sections'            => settings_sections(),
 				'premium_sections'    => PREMIUM_SECTIONS,
 				'premium_url_prefix'  => PREMIUM_URL_PREFIX,
@@ -32,10 +31,6 @@ class Base {
 
 	public function get_settings_instance(): Settings {
 		return $this->settings_instance;
-	}
-
-	public function get_settings_data(): array {
-		return $this->settings_data;
 	}
 
 	public function get_errors(): \WP_Error {
