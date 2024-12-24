466c59ec2c7fb90d8d958ccee671063e2a9cbb3d
Nicolas Jonas <nnico+git@pm.me>
2024-12-24 03:29:36 +0100

    no glob

--- vendor/nextgenthemes/wp-shared/includes/WP/load-files.php
+++ vendor/nextgenthemes/wp-shared/includes/WP/load-files.php
@@ -4,10 +4,13 @@ declare(strict_types = 1);
 
 namespace Nextgenthemes\WP;
 
-foreach ( glob(__DIR__ . '/fn-*.php') as $filename ) {
-	require_once $filename;
-}
-
+require_once __DIR__ . '/fn-asset-helpers.php';
+require_once __DIR__ . '/fn-license.php';
+require_once __DIR__ . '/fn-misc.php';
+require_once __DIR__ . '/fn-remote-get.php';
+require_once __DIR__ . '/fn-settings.php';
+require_once __DIR__ . '/fn-string.php';
 require_once __DIR__ . '/Asset.php';
 require_once __DIR__ . '/Settings.php';
+require_once __DIR__ . '/SettingsData.php';
 require_once __DIR__ . '/SettingValidator.php';
