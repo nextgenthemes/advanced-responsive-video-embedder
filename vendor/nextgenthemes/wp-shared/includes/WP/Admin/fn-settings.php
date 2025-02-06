077c3455de8ee8f2e8fde36549f10412a793b6fc
Nicolas Jonas <nnico+git@pm.me>
2025-02-06 23:09:56 +0100

    remove unused fns, use Tag Processor

--- vendor/nextgenthemes/wp-shared/includes/WP/Admin/fn-settings.php
+++ vendor/nextgenthemes/wp-shared/includes/WP/Admin/fn-settings.php
@@ -8,8 +8,7 @@ use Nextgenthemes\WP\SettingsData;
 use Nextgenthemes\WP\SettingValidator;
 
 use function Nextgenthemes\WP\get_defined_key;
-use function Nextgenthemes\WP\has_valid_key;
-use function Nextgenthemes\WP\attr;
+use function Nextgenthemes\WP\create_element;
 use function Nextgenthemes\WP\kses_https_links;
 use function wp_interactivity_data_wp_context as data_wp_context; // This is actually a deprecated function but we use the real one. Avoiding the deprecation warning and the awful long function name.
 
@@ -24,137 +23,6 @@ const DESCRIPTION_ALLOWED_HTML = array(
 	'strong' => array(),
 	'code'   => array(),
 );
-
-function print_boolean_field( string $key, array $option ): void {
-	?>
-	<p>
-		<label>
-			<input
-				type="checkbox"
-				x-model="<?php echo esc_attr( "options.$key" ); ?>"
-			>
-			<?php label_text( $option ); ?>
-		</label>
-	</p>
-	<?php
-}
-
-function print_boolean_radio_field( string $key, array $option ): void {
-	?>
-	<p>
-		<?php label_text( $option ); ?>
-		<label>
-			<input
-				type="radio"
-				x-model="<?php echo esc_attr( "options.$key" ); ?>"
-				name="<?php echo esc_attr( "options.$key" ); ?>"
-			>
-			Yes
-		</label>
-		&nbsp;&nbsp;&nbsp;
-		<label>
-			<input
-				type="radio"
-				x-model="<?php echo esc_attr( "options.$key" ); ?>"
-				name="<?php echo esc_attr( "options.$key" ); ?>"
-			>
-			No
-		</label>
-	</p>
-	<?php
-}
-
-function print_string_field( string $key, array $option ): void {
-	?>
-	<p>
-		<label>
-			<?php label_text( $option ); ?>
-			<input
-				x-model.debounce="<?php echo esc_attr( "options.$key" ); ?>"
-				type="text"
-				class="large-text"
-				placeholder="<?php echo esc_attr( $option['placeholder'] ); ?>"
-			/>
-		</label>
-	</p>
-	<?php
-}
-
-function print_hidden_field( string $key, array $option ): void {} // yes we need this nothing function
-
-function print_old_hidden_field( string $key, array $option ): void {
-	?>
-	<input x-model="<?php echo esc_attr( "options.$key" ); ?>" type="hidden" />
-	<?php
-}
-
-function print_license_key_field( string $key, array $option ): void {
-
-	$readonly = get_defined_key( $key ) ? 'readonly' : '';
-	?>
-	<p>
-		<label>
-			<?php label_text( $option ); ?>
-			<input x-model="<?php echo esc_attr( "options.$key" ); ?>"
-				type="text" class="medium-text" 
-				style="width: 350px;" 
-				<?php echo esc_attr( $readonly ); ?>
-			/>
-			<?php if ( has_valid_key( $key ) ) : ?>
-				<button @click="licenseKeyAction( 'deactivate', '<?php echo esc_attr( $key ); ?>' )" class="button button-secondary">Deactivate</button>
-			<?php else : ?>
-				<button @click="licenseKeyAction( 'activate', '<?php echo esc_attr( $key ); ?>' )" class="button button-secondary">Activate</button>
-			<?php endif; ?>
-			<br>
-			Status: <span x-text="<?php echo esc_attr( "options.{$key}_status" ); ?>"></span>
-		</label>
-	</p>
-	<?php
-}
-
-function print_image_upload_field( string $key, array $option ): void {
-	wp_enqueue_script( 'jquery' );
-	wp_enqueue_media();
-	?>
-	<p>
-		<label>
-			<?php label_text( $option ); ?>
-			<input
-				x-model="<?php echo esc_attr( "options.$key" ); ?>"
-				type="text"
-				class="large-text"
-				placeholder="<?php echo esc_attr( $option['placeholder'] ); ?>"
-			/>
-			<button class="button-secondary" @click="<?php echo esc_attr( "uploadImage('$key')" ); ?>">
-				<?php esc_html_e( 'Upload Image', 'advanced-responsive-video-embedder' ); ?>
-			</button>
-		</label>
-	</p>
-	<?php
-}
-
-function print_select_field( string $key, array $option ): void {
-	?>
-	<p>
-		<label>
-			<?php label_text( $option ); ?>
-			<select x-model="<?php echo esc_attr( "options.$key" ); ?>" >
-				<?php
-				$first = true;
-				foreach ( $option['options'] as $k => $v ) :
-					?>
-					<option value="<?php echo esc_attr( $k ); ?>" <?php echo $first ? 'selected' : ''; ?>>
-						<?php echo esc_html( $v ); ?>
-					</option>
-					<?php
-					$first = false;
-				endforeach;
-				?>
-			</select>
-		</label>
-	</p>
-	<?php
-}
 
 /**
  * Prints all settings blocks.
@@ -222,43 +90,39 @@ function option_block( string $key, SettingValidator $setting, array $tabs ): vo
 					</select>
 					<?php
 				} elseif ( 'checkbox' === $setting->ui_element_type ) {
-					printf(
-						'<input %s/>',
-						attr( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-							array(
-								'type'                   => 'checkbox',
-								'id'                     => $input_id,
-								'data-wp-on--change'     => 'actions.checkboxChange',
-								'data-wp-bind--checked'  => "state.options.$key",
-								'placeholder'            => $setting->placeholder,
-								'class'                  => 'form-check-input',
-								'data-wp-bind--disabled' => 'state.isSaving',
-							)
-						),
+					echo create_element( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+						'<input>',
+						array(
+							'type'                   => 'checkbox',
+							'id'                     => $input_id,
+							'data-wp-on--change'     => 'actions.checkboxChange',
+							'data-wp-bind--checked'  => "state.options.$key",
+							'placeholder'            => $setting->placeholder,
+							'class'                  => 'form-check-input',
+							'data-wp-bind--disabled' => 'state.isSaving',
+						)
 					);
 					label( $input_id, $setting, $tabs );
 				} else {
 					label( $input_id, $setting, $tabs );
 
-					printf(
-						'<input %s/>',
-						attr( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-							array(
-								'type'                   => $setting->ui_element_type,
-								'id'                     => $input_id,
-								'data-wp-on--keyup'      => 'actions.inputChange',
-								'data-wp-on--change'     => 'actions.inputChange',
-								// 'data-arve-url'       => ( 'url' === $key ), // TODO: remove
-								// 'data-wp-context'     => ( 'url' === $key ) ? 'url' : false,
-								'data-wp-bind--value'    => "state.options.$key",
-								'placeholder'            => $setting->placeholder,
-								'class'                  => ( 'license_key' === $setting->ui ) ?
-									'large-text text-large--ngt-key' :
-									'large-text',
-								'maxlength'              => ( 'license_key' === $setting->ui ) ? 32 : false,
-								'data-wp-bind--readonly' => 'state.isSaving',
-								'readonly'               => ( 'license_key' === $setting->ui && get_defined_key( $key ) ) ? 'readonly' : false,
-							)
+					echo create_element( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+						'<input>',
+						array(
+							'type'                   => $setting->ui_element_type,
+							'id'                     => $input_id,
+							'data-wp-on--keyup'      => 'actions.inputChange',
+							'data-wp-on--change'     => 'actions.inputChange',
+							// 'data-arve-url'       => ( 'url' === $key ), // TODO: remove
+							// 'data-wp-context'     => ( 'url' === $key ) ? 'url' : false,
+							'data-wp-bind--value'    => "state.options.$key",
+							'placeholder'            => $setting->placeholder,
+							'class'                  => ( 'license_key' === $setting->ui ) ?
+								'large-text text-large--ngt-key' :
+								'large-text',
+							'maxlength'              => ( 'license_key' === $setting->ui ) ? 32 : false,
+							'data-wp-bind--readonly' => 'state.isSaving',
+							'readonly'               => ( 'license_key' === $setting->ui && get_defined_key( $key ) ) ? 'readonly' : false,
 						)
 					);
 
