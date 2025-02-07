e1257d9919f1dc15616559d2fc91d08ea3ab67a6
Nicolas Jonas <nnico+git@pm.me>
2025-02-07 13:38:03 +0100

    build

--- vendor/composer/jetpack_autoload_filemap.php
+++ vendor/composer/jetpack_autoload_filemap.php
@@ -7,7 +7,7 @@ $baseDir   = dirname($vendorDir);
 
 return array(
 	'47777597fbe681453e41ea4bf4ad9988' => array(
-		'version' => '2025.02.07.08.56.43',
+		'version' => '2025.02.07.12.38.02',
 		'path'    => $vendorDir . '/nextgenthemes/wp-shared/includes/WP/init.php'
 	),
 );
