48b4189c30308de86e7ab6f18e2bcb4dfb079f24
Nicolas Jonas <nnico+git@pm.me>
2025-10-20 04:41:14 +0200

    phpstan

--- tests/phpstan-fixes.php
+++ tests/phpstan-fixes.php
@@ -2,16 +2,6 @@
 
 declare(strict_types = 1);
 
-define( 'WP_DEBUG', false );
-define( 'WP_PLUGIN_DIR', '/srv/wp/plugins' );
-
-define( 'MINUTE_IN_SECONDS', 60 );
-define( 'HOUR_IN_SECONDS', 60 * MINUTE_IN_SECONDS );
-define( 'DAY_IN_SECONDS', 24 * HOUR_IN_SECONDS );
-define( 'WEEK_IN_SECONDS', 7 * DAY_IN_SECONDS );
-define( 'MONTH_IN_SECONDS', 30 * DAY_IN_SECONDS );
-define( 'YEAR_IN_SECONDS', 365 * DAY_IN_SECONDS );
-
 /**
  * @see https://github.com/wp-shortcake/Shortcake/blob/master/dev.php
  *
