3c2df761b08a2abefbe7e704e697c8d005fb2c65
Nicolas Jonas <nnico+git@pm.me>
2026-06-26 12:47:52 +0200

    Fix build script for pnpm/deno compat, rename build:assets to build-assets

--- bin/build.sh
+++ bin/build.sh
@@ -3,5 +3,11 @@ set -Eeuo pipefail
 
 wp @sdev arve block_json > src/block/block.json.new
 mv src/block/block.json.new src/block/block.json
+
 ./bin/build-readme.php
-pnpm run --filter @nextgenthemes/advanced-responsive-video-embedder build:assets
+
+if [[ "${npm_execpath:-}" == *deno ]]; then
+	deno run build-assets
+else
+	pnpm run build-assets
+fi
