819a4521c0f67d75841021b73f82c42419c9d1f1
Nicolas Jonas <nnico+git@pm.me>
2026-08-05 15:41:02 +0200

    [gt.nu] release 10.9.2

---
+++ build/block/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '8032ac89676f1a1c9eaa');
